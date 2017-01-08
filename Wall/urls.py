50daf4c8ee79c5b0fbeb8a6e1b49eeade2afd8f9
于滔滔 <2622355408@qq.com>
2017-01-08 22:24:59 +0800

    Add files via upload

---
+++ Wall/urls.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*- 
+from django.conf.urls import url,include
+from django.contrib import admin
+
+from FundPart.forms import BootstrapAuthenticationForm
+from FundPart.views import *
+from FundPart.admin import *
+from FundPart.FundPartUrls import *
+
+
+from datetime import datetime
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/',include(admin.site.urls)),
+    # url(r'^$', 'FundPart.views.home', name='home'),
+    # url(r'^contact$', 'FundPart.views.contact', name='contact'),
+    # url(r'^about', 'FundPart.views.about', name='about'),
+    url(r'^login/$',
+       'django.contrib.auth.views.login',
+       {
+           'template_name': 'FundPages/login.html',
+           'authentication_form': BootstrapAuthenticationForm,
+           'extra_context':
+           {
+               'title': 'Log in',
+               'year': datetime.now().year,
+           }
+       },
+       name='login'),
+    url(r'^logout$',
+       'django.contrib.auth.views.logout',
+       {
+           'next_page': '/index/1',
+       },
+       name='logout'),
+    url(r'^fundpart/',include('FundPart.FundPartUrls')),
+]
+
