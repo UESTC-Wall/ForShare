2133fab9bb34ce7f637b05f39437e1decd0f8695
于滔滔 <2622355408@qq.com>
2017-01-21 22:44:16 +0800

    Add files via upload

--- Wall/settings.py
+++ Wall/settings.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'FundPart',
     'rest_framework',
-    'rest_framework_docs',
+    'rest_framework.authtoken',
+    'rest_framework_docs'
+
     # 'rest_framework_swagger',
     # 'xadmin',
     # 'crispy_forms',
@@ -53,23 +55,26 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'Wall.urls'
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+# TEMPLATE_DIRS = (
+#     '/Wall/FundPart/templates',
+# )
+
+# TEMPLATES = [
+# {
+#     'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#     # 'DIRS': [],
+#     'APP_DIRS': True,
+#     'OPTIONS': {
+#         'context_processors': [
+#             'django.template.context_processors.debug',
+#             'django.template.context_processors.request',
+#             'django.contrib.auth.context_processors.auth',
+#             'django.contrib.messages.context_processors.messages',
+#         ],
+#     },
+# },
+# ]
 
 
 WSGI_APPLICATION = 'Wall.wsgi.application'
