c89143dfaabd5efd0a5ca35edd350db03c13b56c
于滔滔 <2622355408@qq.com>
2017-04-06 16:13:27 +0800

    Delete admin.py

--- FundPart/admin.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*- 
-from django.contrib import admin
-from FundPart.models import UserList,UrlPublish,UrlComment
-
-admin.site.register(UserList)
-admin.site.register(UrlPublish)
-admin.site.register(UrlComment)
